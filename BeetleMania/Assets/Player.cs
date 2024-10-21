4d7136bef5e78eff1ae8c0311bd2f3465306e226
EvanFred1 <165928866+EvanFred1@users.noreply.github.com>
2024-10-21 13:48:25 -0400

    Added states
    Player gets downed and had invincibility after

--- BeetleMania/Assets/Player.cs
+++ BeetleMania/Assets/Player.cs
@@ -14,6 +14,18 @@ public class Player : MonoBehaviour
     private float shootTimer = 0.0f;
     [HideInInspector] public int bulletCounter = 0;
     private int MaxBullets = 5;
+    enum States
+    {
+        Move,
+        Downed
+    }
+    States state = States.Move;
+    private int pressesNeeded = 5;
+    private int presses = 0;
+
+    private bool isInvincible = false;
+    private float invincibleTimer = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,16 +36,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        inputVec = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
-        if (Input.GetKeyDown(KeyCode.Space) && shootTimer <= 0 && bulletCounter < MaxBullets)
+        switch (state)
         {
-            SpawnBullet();
-            shootTimer = rateOfFire;
-            bulletCounter++;
-        }
-        if (shootTimer > 0)
-        {
-            shootTimer -= Time.deltaTime;
+            case States.Move:
+                StateMove();
+                break;
+            case States.Downed:
+                StateDowned();
+                break;
         }
     }
     private void FixedUpdate()
@@ -46,5 +56,60 @@ public class Player : MonoBehaviour
         newBullet.GetComponent<Bullet>().SetUp(Vector3.up);
         newBullet.transform.position = transform.position;
         newBullet.GetComponent<Bullet>().player = this;
+    }
+    private void StateMove()
+    {
+        inputVec = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        if (Input.GetKeyDown(KeyCode.Space) && shootTimer <= 0 && bulletCounter < MaxBullets)
+        {
+            SpawnBullet();
+            shootTimer = rateOfFire;
+            bulletCounter++;
+        }
+        if (shootTimer > 0)
+        {
+            shootTimer -= Time.deltaTime;
+        }
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer <= 0)
+            {
+                isInvincible = false;
+                GetComponent<SpriteRenderer>().color = Color.white;
+            }
+        }
+    }
+    private void StateDowned()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            {
+            presses++;
+            }
+        if(presses>= pressesNeeded)
+        {
+            state = States.Move;
+            presses = 0;
+            pressesNeeded += 2;
+            pressesNeeded = Mathf.Min(pressesNeeded, 25);
+
+            GetComponent<SpriteRenderer>().color = Color.yellow;
+            isInvincible = true;
+            invincibleTimer = 1.5f;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Shell" && !isInvincible)
+        {
+            state = States.Downed;
+
+            rb.velocity = Vector3.zero;
+            inputVec = Vector3.zero;
+
+            GetComponent<SpriteRenderer>().color = Color.gray;
+
+            shootTimer = 0;
+        }
     }
 }
